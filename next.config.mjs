79bbc31d14698710bb5fc747342eb0ded5b3121f
Heidi Marsh <hmarsh@bellese.io>
2024-05-01 10:18:02 -0400

    adding deployment workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = { output: "export", basePath: "/portfolio-2024" };
 
 export default nextConfig;
