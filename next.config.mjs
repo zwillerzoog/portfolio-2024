29f227ac7783cb9f4974e4504e16e7da7bf6e180
Heidi Marsh <marsh.heidi.c@gmail.com>
2024-06-03 14:32:25 -0400

    adding bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-export default nextConfig;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {};
+
+export default withBundleAnalyzer(nextConfig);
