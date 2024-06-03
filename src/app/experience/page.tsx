e8f5d239de137798db843c31112202102a309697
Heidi Marsh <marsh.heidi.c@gmail.com>
2024-06-03 17:39:19 -0400

    reapplying background img

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -2,7 +2,7 @@ import Work from "../../components/work";
 
 export default function Experience() {
   return (
-    <div className="">
+    <div className="bg-greyLight">
       <div className="fade-in m-auto w-full md:top-0 top-[75px] bottom-0 overflow-y-auto pr-5 pl-5">
         <Work />
       </div>
